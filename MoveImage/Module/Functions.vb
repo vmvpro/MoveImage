61c074dfbc20149f2b9c35312cdca139073f5107
vmvpro <melnvetal@gmail.com>
2020-03-06 00:27:48 +0200

    Настройка входа для тестирования

--- MoveImage/Module/Functions.vb
+++ MoveImage/Module/Functions.vb
@@ -258,23 +258,23 @@ Module Functions
             'sw.Close()
         End If
 
-        If Environment.UserName = "Vetal" Then
+        If Environment.UserName.ToUpper() = "Vetal" Then
             path_Scaner = "E:\Document\Мои рисунки\"
             path_SaveDocuments = "d:\Doc\Work\1_MyApplication\MoveImage\MoveImage (Git)\0_DirectoryFilesTestMoveImage\"
             path_DataBase = "d:\Doc\Work\Работа\База данных\TestDB\"
-        ElseIf Environment.UserName = "MWW54001_" Then
+        ElseIf Environment.UserName.ToUpper() = "MWW54001" Then
             path_Scaner = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\"
             path_SaveDocuments = "\\erp\0_\0_App\540\"
             path_DataBase = "\\erp\0_\0_App\DataBaseAccess\DataBase\"
         Else
-            'sr = New StreamReader("Path.dat")
+            sr = New StreamReader("Path.dat")
 
-            'path_Scaner = sr.ReadLine()
-            'path_SaveDocuments = sr.ReadLine()
-            'path_DataBase = sr.ReadLine()
+            path_Scaner = sr.ReadLine()
+            path_SaveDocuments = sr.ReadLine()
+            path_DataBase = sr.ReadLine()
 
-            'sr.Close()
-            
+            sr.Close()
+
         End If
 
         'Public path_Scaner As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
